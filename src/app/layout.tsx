e00c0af769d82542eb1c4c31aaae316ca8eb7768
hyunwoo <khj930410@naver.com>
2023-12-17 20:50:25 +0900

    chore: normalize css 설치&적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import 'normalize.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
