e9349161c4990983cbbca3920b58284d90aa4276
hyunwoo <khj930410@naver.com>
2023-12-26 00:30:22 +0900

    feat: authContext 작성

---
+++ src/constant/constant.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_AUTH = {
+  uid: '',
+  email: '',
+  displayName: '',
+}
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,12 +1,23 @@
 'use client'
 
+import { child } from 'firebase/database'
 import { SessionProvider } from 'next-auth/react'
-interface AuthContextProps {
-  children: React.ReactNode
-}
 
-const AuthContext = ({ children }: AuthContextProps) => {
-  return <SessionProvider>{children}</SessionProvider>
-}
+// interface AuthContextProps {
+//   children: React.ReactNode
+// }
 
-export default AuthContext
+// const AuthContext = ({ children }: AuthContextProps) => {
+//   return <SessionProvider>{children}</SessionProvider>
+// }
+
+// export default AuthContext
+import { AuthContextProps } from '@/type/type'
+import { createContext } from 'react'
+
+import { DEFAULT_AUTH } from '@/constant/constant'
+
+export const AuthContext = createContext<AuthContextProps | null>(DEFAULT_AUTH)
+export const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+  return <AuthContext.Provider value={DEFAULT_AUTH}>{children}</AuthContext.Provider>
+}
