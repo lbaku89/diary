b306fd39f0a3c2620e89ddafc9e5a2520a2f10a1
hyunwoo <khj930410@naver.com>
2024-04-16 21:26:01 +0900

    etc: 불필요한 주석 삭제, 주석 추가

--- src/shared/components/BackBtn.tsx
+++ src/shared/components/BackBtn.tsx
@@ -1,23 +1,13 @@
-'use client'
-
-// * import from next
 import Link from 'next/link'
-
-// * import component
 import { IconButton } from '@mui/material'
-
-// * import icon
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 
 export default function BackBtn({ pathName }: { pathName: string }) {
   return (
-    <>
-      {/* 뒤로가기 */}
-      <Link href={{ pathname: `${pathName}` }}>
-        <IconButton>
-          <ArrowBackIcon />
-        </IconButton>
-      </Link>
-    </>
+    <Link href={{ pathname: `${pathName}` }}>
+      <IconButton>
+        <ArrowBackIcon />
+      </IconButton>
+    </Link>
   )
 }
