f787f238fb0b48910848e783d5af2697f37fa087
hyunwoo <khj930410@naver.com>
2024-04-17 23:42:32 +0900

    refactor: state 제거 query string 만으로 날짤 컨트롤

--- src/app/(main)/_component/CalendarControlUi.tsx
+++ src/app/(main)/_component/CalendarControlUi.tsx
@@ -4,22 +4,32 @@ import { Box, Typography, IconButton, Button } from '@mui/material'
 import { getTodayInfo } from '@/shared/utils/getCalendarInfo'
 import ArrowBackIosNewSharpIcon from '@mui/icons-material/ArrowBackIosNewSharp'
 import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp'
-import { useRouter, usePathname } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useRouter, usePathname, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
 import { MonthIndex } from '@/shared/types/type'
 
 export default function CalendarControlUI() {
-  const [todayInfo, pathname, router] = [getTodayInfo(), usePathname(), useRouter()]
-  const [selected, setSelected] = useState<{ year: number; monthIndex: MonthIndex }>({
-    year: todayInfo.year,
-    monthIndex: todayInfo.monthIndex,
-  })
-
+  const [todayInfo, pathname, router, searchParams] = [getTodayInfo(), usePathname(), useRouter(), useSearchParams()]
+  const isQueryStringExist = searchParams.get('year') && searchParams.get('month')
   useEffect(() => {
-    router.replace(`${pathname}?year=${todayInfo.year}&month=${todayInfo.monthIndex + 1}`, {
-      scroll: false,
-    })
-  }, [pathname, router, todayInfo.monthIndex, todayInfo.year])
+    if (!isQueryStringExist) {
+      // 첫 렌더링시 querystring이 없을 경우 today info로 querystring params 세팅
+      router.replace(`${pathname}?year=${todayInfo.year}&month=${todayInfo.monthIndex + 1}`, {
+        scroll: false,
+      })
+    }
+  }, [isQueryStringExist, pathname, router, todayInfo.monthIndex, todayInfo.year])
+
+  const selected = {
+    year: Number(searchParams.get('year')) || todayInfo.year,
+    month: Number(searchParams.get('month')) || todayInfo.monthIndex + 1,
+  }
+
+  const currentYearMonthQueryParams = () => ({
+    year: Number(searchParams.get('year')),
+    month: Number(searchParams.get('month')),
+    monthIndex: (Number(searchParams.get('month')) - 1) as MonthIndex,
+  })
 
   const updateQueryString = ({ year, month }: { year: number; month: number }) => {
     router.replace(`${pathname}?year=${year}&month=${month}`, {
@@ -29,20 +39,19 @@ export default function CalendarControlUI() {
 
   const handleClickTodayBtn = () => {
     updateQueryString({ year: todayInfo.year, month: todayInfo.monthIndex + 1 })
-    setSelected({ year: todayInfo.year, monthIndex: todayInfo.monthIndex })
   }
 
   const handleClickPrevBtn = () => {
-    const newYear = selected.monthIndex === 0 ? selected.year - 1 : selected.year
-    const newMonthIndex = selected.monthIndex === 0 ? 11 : ((selected.monthIndex - 1) as MonthIndex)
-    setSelected({ year: newYear, monthIndex: newMonthIndex })
+    const { year, monthIndex } = currentYearMonthQueryParams()
+    const newYear = monthIndex === 0 ? year - 1 : year
+    const newMonthIndex = monthIndex === 0 ? 11 : ((monthIndex - 1) as MonthIndex)
     updateQueryString({ year: newYear, month: newMonthIndex + 1 })
   }
 
   const handleClickNextBtn = () => {
-    const newYear = selected.monthIndex === 11 ? selected.year + 1 : selected.year
-    const newMonthIndex = selected.monthIndex === 11 ? 0 : ((selected.monthIndex + 1) as MonthIndex)
-    setSelected({ year: newYear, monthIndex: newMonthIndex })
+    const { year, monthIndex } = currentYearMonthQueryParams()
+    const newYear = monthIndex === 11 ? year + 1 : year
+    const newMonthIndex = monthIndex === 11 ? 0 : ((monthIndex + 1) as MonthIndex)
     updateQueryString({ year: newYear, month: newMonthIndex + 1 })
   }
 
@@ -74,7 +83,7 @@ export default function CalendarControlUI() {
         <ArrowForwardIosSharpIcon fontSize="inherit" />
       </IconButton>
       <Typography>
-        {selected.year}년 {selected.monthIndex + 1}월
+        {selected.year}년 {selected.month}월
       </Typography>
     </Box>
   )
