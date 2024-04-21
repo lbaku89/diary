46f42754737731f246646a25f7ad90649dfcac95
hyunwoo <khj930410@naver.com>
2024-04-21 16:49:13 +0900

    refactor: context 변경되는 동안 대체 ui 처리

--- src/app/(main)/_component/UserName.tsx
+++ src/app/(main)/_component/UserName.tsx
@@ -1,16 +1,19 @@
 'use client'
 
-import { Typography } from '@mui/material'
+import { Skeleton, Typography } from '@mui/material'
 import { useContext } from 'react'
 import AuthContext from '@/shared/context/AuthContext'
 
-// todo 로딩중 스켈레톤 처리 (dynamic import)
 export default function UserName() {
   const name = useContext(AuthContext)?.authContextValue?.displayName
 
-  return (
-    <Typography variant="h6" sx={{ marginBottom: '0.5rem' }}>
-      {name}님의 다이어리
-    </Typography>
-  )
+  if (name) {
+    return (
+      <Typography variant="h6" sx={{ marginBottom: '0.5rem' }}>
+        {name}님의 다이어리
+      </Typography>
+    )
+  }
+
+  return <Skeleton variant="text" sx={{ fontSize: '1.5rem', width: '200px', marginBottom: '0.5rem' }} />
 }
