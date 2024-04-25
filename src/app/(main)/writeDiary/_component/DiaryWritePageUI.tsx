61326b596c393e6d8ba8a3cc70e0875de841a1fa
hyunwoo <khj930410@naver.com>
2024-04-25 21:57:58 +0900

    refactor: 이벤트핸들러 별도 분리 및 공통컴포넌트로 대체

--- src/app/(main)/writeDiary/_component/DiaryWritePageUI.tsx
+++ src/app/(main)/writeDiary/_component/DiaryWritePageUI.tsx
@@ -1,24 +1,13 @@
 'use client'
 
-// * import from next
-import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-
-// * import component
 import { Button, TextField, Box } from '@mui/material'
-
-// * import Type
 import { Day } from '@/shared/types/type'
-
-// * import context
 import AuthContext from '@/shared/context/AuthContext'
 import { useContext, useState } from 'react'
-
-// * import utils
 import Utils from '@/shared/utils/utility'
-
-// * import api
 import { addDiary } from '@/shared/api/api'
+import CancelBtn from '@/shared/components/CancelBtn'
 
 export default function DiaryWritePageUI({
   year,
@@ -32,11 +21,46 @@ export default function DiaryWritePageUI({
   day: Day
 }) {
   const { authContextValue } = useContext(AuthContext)!
-  const route = useRouter()
+  const router = useRouter()
   const [title, setTitle] = useState<string>('')
   const [content, setContent] = useState<string>('')
   const [isTitleError, setIsTitleError] = useState<boolean>(false)
   const [isContentError, setIsContentError] = useState<boolean>(false)
+
+  const handleClickWriteDiaryBtn = () => {
+    const [isEmptyTitle, isEmptyContent] = [Utils.isEmptyText(title), Utils.isEmptyText(content)]
+
+    if (isEmptyTitle) {
+      setIsTitleError(true)
+    } else {
+      setIsTitleError(false)
+    }
+
+    if (isEmptyContent) {
+      setIsContentError(true)
+    } else {
+      setIsContentError(false)
+    }
+
+    if (!isEmptyTitle && !isEmptyContent) {
+      addDiary({
+        uid: authContextValue!.uid,
+        dateInfo: {
+          year: Number(year),
+          month: Number(month),
+          date: Number(date),
+        },
+        diary: { title, content },
+      })
+        .then(() => {
+          alert(`작성완료!`)
+          router.back()
+        })
+        .catch((error) => {
+          alert(`문제가 발생하여 작성에 실패했습니다. error message:${error.message}`)
+        })
+    }
+  }
 
   // todo : 컴포넌트 이름 변경 (제목,내용,버튼을 포함하는 이름으로)
   // todo : 컴포넌트 정리하기
@@ -71,55 +95,11 @@ export default function DiaryWritePageUI({
 
       <Box sx={{ display: 'flex', justifyContent: 'end' }}>
         {/* 작성 btn */}
-        <Button
-          variant="contained"
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault()
-
-            const [isEmptyTitle, isEmptyContent] = [Utils.isEmptyText(title), Utils.isEmptyText(content)]
-
-            if (isEmptyTitle) {
-              setIsTitleError(true)
-            } else {
-              setIsTitleError(false)
-            }
-
-            if (isEmptyContent) {
-              setIsContentError(true)
-            } else {
-              setIsContentError(false)
-            }
-
-            if (!isEmptyTitle && !isEmptyContent) {
-              addDiary({
-                uid: authContextValue!.uid,
-                dateInfo: {
-                  year: Number(year),
-                  month: Number(month),
-                  date: Number(date),
-                },
-                diary: { title, content },
-              })
-                .then(() => {
-                  alert(`작성완료!`)
-                  route.push('/')
-                })
-                .catch((error) => {
-                  alert(`문제가 발생하여 작성에 실패했습니다. error message:${error.message}`)
-                  console.error(error)
-                })
-            }
-          }}
-        >
+        <Button variant="contained" onClick={handleClickWriteDiaryBtn}>
           작성완료
         </Button>
         {/* 취소 btn */}
-        <Link href={{ pathname: '/' }} style={{ marginLeft: '0.5rem' }}>
-          <Button type="button" variant="outlined">
-            취소
-          </Button>
-        </Link>
+        <CancelBtn />
       </Box>
     </Box>
   )
