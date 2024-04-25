e68b2272aca72eb4534fa17f74d4c8d3c7951a5d
hyunwoo <khj930410@naver.com>
2024-04-26 00:24:52 +0900

    refactor: 다이어리 수정 페이지 리팩토링

--- src/app/(main)/modifyDiary/page.tsx
+++ src/app/(main)/modifyDiary/page.tsx
@@ -1,6 +1,25 @@
-// * import components
-import DiaryModifyPageUI from '@/app/(main)/modifyDiary/_component/DiaryModifyPageUI'
+import DiaryContentAndModifyBtn from '@/app/(main)/modifyDiary/_component/DiaryContentAndModifyBtn'
+import { Box, Typography } from '@mui/material'
+import BackBtn from '@/shared/components/BackBtn'
+import Utils from '@/shared/utils/utility'
+import { Day } from '@/shared/types/type'
 
-export default function DiaryModifyPage() {
-  return <DiaryModifyPageUI />
+export default function DiaryModifyPage({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
+  const { year, month, date, day, diaryId } = searchParams
+
+  return (
+    <Box sx={{ padding: '2rem 0' }}>
+      <BackBtn />
+      <Typography variant="h5" sx={{ textAlign: 'center' }}>
+        {year}.{month}.{date} {Utils.convertDayToKorean(day as Day)}
+      </Typography>
+      <DiaryContentAndModifyBtn
+        year={Number(year)}
+        month={Number(month)}
+        date={Number(date)}
+        day={day as Day}
+        diaryId={diaryId}
+      />
+    </Box>
+  )
 }
